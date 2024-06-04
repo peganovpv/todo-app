0020f67087b9e461b18508a1ac1b65b1094ee3be
Peter Peganov <peter@peganov.net>
2024-06-04 19:21:23 +0300

    added all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './config/AuthContext';
+import { ProtectedRoutes } from './config/ProtectedRoutes';
+
+import Home from './Pages/Home';
+
+import Login from './Pages/Auth/Login'
+import Register from './Pages/Auth/Register'
+
+import Todo from './Pages/Todo';
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/todo" element={
+            <ProtectedRoutes>
+              <Todo />
+            </ProtectedRoutes>
+          } />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
